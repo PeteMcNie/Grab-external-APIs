2b8de19ba7f74d4a430c9b84e9afd49cbff451f8
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-06-24 15:24:34 +1200

    Initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import Iss from './Iss'
-import Map from './Map'
 
 const App = () => {
   return (
     <>
-    <h1>Where is the International Space Station (ISS)</h1>
-    <Iss />
-    <Map />
+    <h1></h1>
+
     </>
   )
 }
